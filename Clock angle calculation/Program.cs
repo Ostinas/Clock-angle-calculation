62478dbcfb8af35f404683e647ea6ee2ccafbb25
Ostinas <ostinasmaciulis@gmail.com>
2022-09-19 15:03:45 +0300

    Wrote logic for calculating angle and for printing results.

--- Clock angle calculation/Program.cs
+++ Clock angle calculation/Program.cs
@@ -1,4 +1,27 @@
-﻿Console.WriteLine("Hello, please input your desired hours (in 24h format): ");
+﻿using System.Transactions;
+
+Console.WriteLine("Hello, please input your desired hours (in 24h format): ");
 int Hours = int.Parse(Console.ReadLine());
 Console.WriteLine("Please input your desired minutes: ");
-var Minutes = Console.ReadLine();
+int Minutes = int.Parse(Console.ReadLine());
+Console.WriteLine("The time that you have chosen: {0}:{1}", Hours, Minutes);
+
+
+// minute arrow moves by 6 degrees each minute
+int LesserAngle = 6;
+int MinAngle = LesserAngle * Minutes;
+
+double HrsAngle = Hours * 30 + Minutes * 0.5;
+
+double Angle = HrsAngle - MinAngle;
+switch(Angle){
+    case > 180.0:
+        Console.WriteLine("The lesser angle between the arrows is: {0}", 360 - Angle);
+        break;
+    case <= 180.0:
+        Console.WriteLine("The lesser angle between the arrows is: {0}", Angle);
+        break;
+    default:
+        Console.WriteLine("The angles between both sides of the arrows are equal");
+        break;
+}
