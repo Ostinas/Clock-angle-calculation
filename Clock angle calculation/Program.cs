55576ed042c46163e74bd0ab77f5f7aa737adf1c
Ostinas <ostinasmaciulis@gmail.com>
2022-09-19 14:46:44 +0300

    Added input lines

--- Clock angle calculation/Program.cs
+++ Clock angle calculation/Program.cs
@@ -1,2 +1,4 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, please input your desired hours (in 24h format): ");
+int Hours = int.Parse(Console.ReadLine());
+Console.WriteLine("Please input your desired minutes: ");
+var Minutes = Console.ReadLine();
